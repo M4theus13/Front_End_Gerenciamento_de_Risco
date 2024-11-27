0b8dcbdee9c9108d15eab49c0a195af430c5a844
Matheus Ti <matheusgarciajbs@gmail.com>
2024-11-27 12:56:07 -0300

    verificacao de input vazio /cadastro

--- src/pages/cadastrar/cadastrar.jsx
+++ src/pages/cadastrar/cadastrar.jsx
@@ -10,17 +10,51 @@ function cadastrar() {
   const inputEmail = useRef()
   const inputPassword = useRef()
 
-  async function createUser() {
-    try {
+  const textName = useRef()
+  const textEmail = useRef()
+  const textPassword = useRef()
 
-      await api.post('/usuarios/cadastro', {
-        name: inputName.current.value,
-        email: inputEmail.current.value,
-        password: inputPassword.current.value
-      })
-      console.log(api)
-    } catch (err) {
-      console.log(err)
+  const inputs = [
+    {ref: inputName, textRef: textName},
+    {ref: inputEmail, textRef: textEmail},
+    {ref: inputPassword, textRef: textPassword}
+  ]
+
+  async function createUser() {
+    let hasError = false
+    inputs.forEach((input) => { 
+      const valueInput = input.ref.current.value.trim()
+      if (!valueInput) {
+        input.ref.current.classList.add('errorEmptyInput')
+        input.textRef.current.classList.remove('okText')
+        input.textRef.current.classList.add('errorEmptyText')
+      } else {
+        input.ref.current.classList.remove('errorEmptyInput');
+        input.textRef.current.classList.remove('errorEmptyText');
+        input.textRef.current.classList.add('okText');
+      }
+    }) 
+
+    if (!inputName. current.value.trim() || inputEmail.current.value.trim() || inputPassword.current.value.trim() ) {
+      hasError = true
+      return
+    }
+    
+    if (!hasError) {
+
+        try {
+          if (hasError) {
+            
+          }
+          await api.post('/usuarios/cadastro', {
+            name: inputName.current.value,
+          email: inputEmail.current.value,
+          password: inputPassword.current.value
+        })
+        console.log(api)
+      } catch (err) {
+        console.log(err)
+      }
     }
   }
 
@@ -29,8 +63,11 @@ function cadastrar() {
       <Header text='Cadastrar'></Header>
       <div className='box-form'>
         <form>
-          <input id='name' type="text" placeholder='Nome' ref={inputName}/>
+          <span  className='okText' ref={textName}>Nome Vazio</span>
+          <input id='name' placeholder='Nome' ref={inputName}/>
+          <span className='okText' ref={textEmail}>Email Vazio</span>
           <input id='email' type="text" placeholder='Email' ref={inputEmail}/>
+          <span className='okText' ref={textPassword}>Senha Vazia</span>
           <input id='password' type="text" placeholder='Senha' ref={inputPassword}/>
           <button className='button-cadastrar' type='button' onClick={createUser}>Criar</button>
         </form>
