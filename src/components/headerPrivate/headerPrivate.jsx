f08b1983b9842625a6b85e8636ec268b2b1c6416
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-02-12 14:40:00 -0300

    apenas adm ter acesso a pagina adm

--- src/components/headerPrivate/headerPrivate.jsx
+++ src/components/headerPrivate/headerPrivate.jsx
@@ -1,31 +1,30 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './headerPrivate.css'
 import { IoMenu } from "react-icons/io5";
 import { IoClose } from "react-icons/io5";
 import { Link } from 'react-router-dom';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import {GetUserInfo} from '../../../service/getUsers.js'
 import { jwtDecode } from 'jwt-decode';
 
 function headerPrivate({text, name}) {
+  const [userAdminInfo, setUserAdminInfo] = useState(false)
 
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+    if (!token) {
+      console.log('Usuário não encontrado HEader')
+      return
+    } 
+    const decoded = jwtDecode(token); //aqui
+    const userAdminInfo = decoded.admin; // Obtém se é ADMIN do usuário do token decodificado
+    setUserAdminInfo(userAdminInfo)
+  }, [])
+  
   const handdleClick = () => {
     document.getElementById('buttonMenuOpen').classList.toggle('hidden')
     document.getElementById('buttonMenuClose').classList.toggle('hidden')
     document.querySelector('.menuHeader').classList.toggle('open')
     document.querySelector('.menuHeader').classList.toggle('close')
   } 
-
-  const token = localStorage.getItem('token')
-  let [userLogado, setUserLogado] = useState([])//informações do usuario logado para o user info
-  let [usersInfo, setUsersInfo] = useState([]) 
-  
-  useEffect(() => {
-    GetUserInfo(token , setUserLogado, setUsersInfo)
-  }, [])
-
-  const decoded = jwtDecode(token ,process.env.JWT_SECRET); //aqui
 
   return (
     <header className='headerPrivate'>
@@ -43,7 +42,7 @@ function headerPrivate({text, name}) {
       </nav>
 
       <div className='menuHeader close'>
-        {userLogado.Admin ? <Link to='/admin/listar-usuarios'>Administrador</Link> : ''}
+        {userAdminInfo ? <Link to='/admin/listar-usuarios'>Administrador</Link> : ''}
         <p>Perfil</p>
         <p>Configurações</p>
         <p>Sair</p>
--- src/pages/listar-usuarios/getUsuarios.jsx
+++ src/pages/listar-usuarios/getUsuarios.jsx
@@ -2,24 +2,43 @@ import React, { useEffect, useState } from 'react'
 import api from '../../../service/api'
 import HeaderPrivate from '../../components/headerPrivate/headerPrivate'
 import { GetUserInfo }from '../../../service/getUsers'
+import { jwtDecode } from 'jwt-decode'
 import './getUsuarios.css'
+import { useNavigate } from 'react-router-dom'
 
 function GetUsuarios() {
+  const navigate = useNavigate()
+
   let [userLogado, setUserLogado] = useState([])//informações do usuario logado para o user info
   let [usersInfo, setUsersInfo] = useState([]) //informações dos usuarios que estão no site
+  
+  let [userIdInfo, setUserIdInfo] = useState([]) //informações do usuario logado para o user info 
+  let [userAdminInfo, setUserAdminInfo] = useState(false) //informações do usuario logado para o user info  
+
   const token = localStorage.getItem('token')
 
   useEffect(() => {
+    if (!token) {
+      console.log('Usuário não encontrado')
+      navigate('/')
+      return
+    } 
+    const decoded = jwtDecode(token);
+
+    setUserIdInfo(decoded.id) // Obtém o ID do usuário do token decodificado
+    setUserAdminInfo(decoded.admin) // Obtém se é ADMIN do usuário do token decodificado
+    
     GetUserInfo(token, setUserLogado, setUsersInfo)
   }, [])
   
-
   const tornarAdmin = async (userId) => {
     try{
       await api.put(`/admin/add-admin/${userId}`,{} ,{
         headers: {Authorization : `Bearer ${token}`}
+        
       })
       GetUserInfo(token, setUserLogado, setUsersInfo)
+
     } catch (err) {
       console.log(err.message)
     }
@@ -31,6 +50,7 @@ function GetUsuarios() {
         headers: {Authorization : `Bearer ${token}`}
       })
       GetUserInfo(token, setUserLogado, setUsersInfo)
+
     } catch (err) {
       console.log(err.message)
     }
@@ -42,6 +62,7 @@ function GetUsuarios() {
         headers: {Authorization : `Bearer ${token}`}
       })
       GetUserInfo(token, setUserLogado, setUsersInfo)
+
       } catch (err) {
         console.log(err)
       } 
@@ -57,9 +78,9 @@ function GetUsuarios() {
             <div className='user-box'>
               <p>{usersInfo.name}</p>
               <p>{usersInfo.isAdmin ? 'Administrador' : ''}</p>
-              {(userLogado.isAdmin && !usersInfo.isAdmin) ?  <button onClick={() => tornarAdmin(usersInfo.id)}>Tornar administrador</button> : ''}
-              {(userLogado.isAdmin && usersInfo.isAdmin) ?  <button onClick={() => removerAdmin(usersInfo.id)}>Remover administrador</button> : ''}
-              {userLogado.isAdmin ? <button onClick={() => excluirUser(usersInfo.id)}>Excluir</button> : ''}
+              {(userAdminInfo && !usersInfo.isAdmin) ?  <button onClick={() => tornarAdmin(usersInfo.id)}>Tornar administrador</button> : ''}
+              {(userAdminInfo && usersInfo.isAdmin) ?  <button onClick={() => removerAdmin(usersInfo.id)}>Remover administrador</button> : ''}
+              {userAdminInfo ? <button onClick={() => excluirUser(usersInfo.id)}>Deletar Usuário</button> : ''}
             </div>
           </div>
         ))}
