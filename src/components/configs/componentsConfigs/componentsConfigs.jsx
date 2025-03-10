19376f2f146bc89a38dc3dca4eaa545b6213e694
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-03-10 17:08:14 -0300

    validação email e tratamento de erros

--- src/components/configs/componentsConfigs/componentsConfigs.jsx
+++ src/components/configs/componentsConfigs/componentsConfigs.jsx
@@ -6,6 +6,7 @@ import { Navigate, useNavigate } from 'react-router-dom';
 
 function ComponentName() {
   const navigate = useNavigate()
+  let hasError = false
 
   const token = localStorage.getItem('token')
   let [userLogado, setUserLogado] = useState()
@@ -14,19 +15,40 @@ function ComponentName() {
 
   useEffect(() => {
     GetUserInfo(token, setUserLogado, setUserInfo )
+
+    const removeErrorInput = (event) => {
+      event.target.classList.remove('error');
+    };
+
+    inputNewName.current.addEventListener('focus', removeErrorInput)
   }, [])
 
   const inputNewName = useRef()
 
-  async function alterarNome() {
-    await api.put(`/edit-name-user/${userLogado.id}`, {
-      name: inputNewName.current.value
-    }, {
-      headers: {Authorization : `Bearer ${token}`}
-    })
+    async function alterarNome() {
 
-    navigate('/menu')
-  }
+      if (inputNewName.current?.value.trim() === '') {
+        inputNewName.current.placeholder = 'Campo obrigatório'
+        inputNewName.current.classList.toggle('error')
+        hasError = true
+      } else {
+        hasError = false
+      }
+
+      if (!hasError) {
+
+        await api.put(`/edit-name-user/${userLogado.id}`, {
+          name: inputNewName.current.value
+        }, {
+          headers: {Authorization : `Bearer ${token}`}
+        })
+    
+        navigate('/menu')
+
+      } else {
+        console.log('erro')
+      }
+    }
 
   return <div className='componentName'>
     <div className='componentName-current-name'>
@@ -45,8 +67,7 @@ function ComponentName() {
 
 function ComponentEmail() {
 
-  const navigate = useNavigate()
-
+  const navigate = useNavigate()  
   const token = localStorage.getItem('token')
   let [userLogado, setUserLogado] = useState()
   let [usersInfo, setUserInfo] = useState() 
@@ -54,41 +75,89 @@ function ComponentEmail() {
 
   useEffect(() => {
     GetUserInfo(token, setUserLogado, setUserInfo )
+    
+    const removeErrorInput = (event) => {
+      event.target.classList.remove('error');
+    };
+
+    inputEmail.current.addEventListener('focus', removeErrorInput)
+    inputNewEmail.current.addEventListener('focus', removeErrorInput)
   }, [])
 
   const inputEmail = useRef()
   const inputNewEmail = useRef()
 
+  let hasErrorEmail = false
+  let input
   async function alterarEmail() {
-
-    try {
-      const user = await api.post('/usuarios/user' , {
-        email: inputEmail.current.value
-      })
-      if  (user.status === 201) { //se status 201 nao existe esse email
-        console.log('email nao cadastrado ou nao encontrado')
-      }
-      if (user.status === 200) { //se status 200 ja existe usuario com esse email
-        console.log('email encontrado')
-        updateEmail()
-
-      }
-    } catch (err) {
-      console.log(err)
+    //validação se o campo do email novo esta vazio
+    if (inputNewEmail.current?.value.trim() === '') {
+      inputNewEmail.current.placeholder = 'Campo obrigatório'
+      inputNewEmail.current.classList.toggle('error')
+      console.log('teste1')
+    } 
+    //validação se o campo do email atual esta vazio
+    if (inputEmail.current?.value.trim() === '') {
+      inputEmail.current.placeholder = 'Campo obrigatório'
+      inputEmail.current.classList.toggle('error')
+      console.log('teste2')
+    } 
+    //validação se o campo do email atual e novo email estao vazios para nao fazer a requisição
+    if (inputEmail.current?.value.trim() === '' || inputNewEmail.current?.value.trim() === '') {
+      hasErrorEmail = true 
+      console.log('teste3')
+      return
     }
 
-    async function updateEmail() {
+    //validação do email com  @ e .com
+    if  (!inputNewEmail.current?.value.includes('@') || !inputNewEmail.current?.value.includes('.com')) {
+      inputNewEmail.current.placeholder = 'Email deve conter @ e .com'
+      inputNewEmail.current.classList.toggle('error')
+      hasErrorEmail = true
+    } else {
+      hasErrorEmail = false 
+    }
+
+    if (!hasErrorEmail) {
       try {
-        await api.put(`/edit-email-user/${userLogado.id}`, {
-          newEmail: inputNewEmail.current.value
-        }, {
-          headers: {Authorization : `Bearer ${token}`}
+        const user = await api.post('/usuarios/user' , {
+          email: inputEmail.current.value
         })
-      } catch(err) {
+
+        if  (user.status === 201) { //se status 201 nao existe esse email
+          console.log('email nao cadastrado ou nao encontrado')
+          inputEmail.current.placeholder = 'Email Incorreto ou não encontrado'
+          inputEmail.current.classList.toggle('error')
+          hasErrorEmail = true
+          return
+        }
+
+        if (user.status === 200) { //se status 200 ja existe usuario com esse email
+          console.log('email encontrado')
+          if (!hasErrorEmail) {
+            updateEmail()
+            navigate('/menu')
+          }
+
+        }
+      } catch (err) {
         console.log(err)
       }
     }
-    navigate('/menu')
+
+
+      async function updateEmail() {
+        try {
+          await api.put(`/edit-email-user/${userLogado.id}`, {
+            newEmail: inputNewEmail.current.value
+          }, {
+            headers: {Authorization : `Bearer ${token}`}
+          })
+        } catch(err) {
+          console.log(err)
+        }
+      }
+
   }
 
   return <div className='componentEmail'>
