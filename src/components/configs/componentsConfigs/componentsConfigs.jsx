aba548f4aeb0665c776f0fa63ce0eaf8c144ce59
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-03-13 17:11:47 -0300

    validacao email e password

--- src/components/configs/componentsConfigs/componentsConfigs.jsx
+++ src/components/configs/componentsConfigs/componentsConfigs.jsx
@@ -18,6 +18,7 @@ function ComponentName() {
 
     const removeErrorInput = (event) => {
       event.target.classList.remove('error');
+      event.target.placeholder = ''
     };
 
     inputNewName.current.addEventListener('focus', removeErrorInput)
@@ -29,7 +30,7 @@ function ComponentName() {
 
       if (inputNewName.current?.value.trim() === '') {
         inputNewName.current.placeholder = 'Campo obrigatório'
-        inputNewName.current.classList.toggle('error')
+        inputNewName.current.classList.add('error')
         hasError = true
       } else {
         hasError = false
@@ -78,6 +79,7 @@ function ComponentEmail() {
     
     const removeErrorInput = (event) => {
       event.target.classList.remove('error');
+      event.target.placeholder = ''
     };
 
     inputEmail.current.addEventListener('focus', removeErrorInput)
@@ -88,75 +90,72 @@ function ComponentEmail() {
   const inputNewEmail = useRef()
 
   let hasErrorEmail = false
-  let input
   async function alterarEmail() {
     //validação se o campo do email novo esta vazio
     if (inputNewEmail.current?.value.trim() === '') {
       inputNewEmail.current.placeholder = 'Campo obrigatório'
-      inputNewEmail.current.classList.toggle('error')
-      console.log('teste1')
+      inputNewEmail.current.classList.add('error')
     } 
     //validação se o campo do email atual esta vazio
     if (inputEmail.current?.value.trim() === '') {
       inputEmail.current.placeholder = 'Campo obrigatório'
-      inputEmail.current.classList.toggle('error')
-      console.log('teste2')
+      inputEmail.current.classList.add('error')
     } 
     //validação se o campo do email atual e novo email estao vazios para nao fazer a requisição
     if (inputEmail.current?.value.trim() === '' || inputNewEmail.current?.value.trim() === '') {
       hasErrorEmail = true 
-      console.log('teste3')
       return
     }
 
-    //validação do email com  @ e .com
+    //validação se o email novo tem @ e .com
     if  (!inputNewEmail.current?.value.includes('@') || !inputNewEmail.current?.value.includes('.com')) {
+      inputEmail.current.value = ''
       inputNewEmail.current.placeholder = 'Email deve conter @ e .com'
-      inputNewEmail.current.classList.toggle('error')
+      inputNewEmail.current.classList.add('error')
       hasErrorEmail = true
     } else {
       hasErrorEmail = false 
     }
 
+    //validação se o email atual é correspondente ao email do usuario
     if (!hasErrorEmail) {
-      try {
-        const user = await api.post('/usuarios/user' , {
+      //guarda valor do email do usuario que esta no banco de dados
+      try  {
+        const verificaEmail = await api.post(`/verify-email/${userLogado.id}`, {
           email: inputEmail.current.value
+        }, {
+          headers: {Authorization : `Bearer ${token}`}
         })
 
-        if  (user.status === 201) { //se status 201 nao existe esse email
-          console.log('email nao cadastrado ou nao encontrado')
-          inputEmail.current.placeholder = 'Email Incorreto ou não encontrado'
-          inputEmail.current.classList.toggle('error')
+        //verifica se o email atual e o email do banco de dados são iguais
+        if (verificaEmail.data.emailUser[0].email !== inputEmail.current.value) {
+          inputEmail.current.value = ''
+          inputEmail.current.placeholder = 'Email Incorreto'
+          inputEmail.current.classList.add('error')
           hasErrorEmail = true
           return
-        }
-
-        if (user.status === 200) { //se status 200 ja existe usuario com esse email
-          console.log('email encontrado')
-          if (!hasErrorEmail) {
-            updateEmail()
-            navigate('/menu')
-          }
-
         }
       } catch (err) {
         console.log(err)
       }
     }
 
+    if (!hasErrorEmail) {
+      updateEmail()
+      navigate('/menu')
+    }
 
-      async function updateEmail() {
-        try {
-          await api.put(`/edit-email-user/${userLogado.id}`, {
-            newEmail: inputNewEmail.current.value
-          }, {
-            headers: {Authorization : `Bearer ${token}`}
-          })
-        } catch(err) {
-          console.log(err)
-        }
+    async function updateEmail() {
+      try {
+        await api.put(`/edit-email-user/${userLogado.id}`, {
+          newEmail: inputNewEmail.current.value
+        }, {
+          headers: {Authorization : `Bearer ${token}`}
+        })
+      } catch(err) {
+        console.log(err)
       }
+    }
 
   }
 
@@ -185,29 +184,104 @@ function ComponentPassword() {
 
   useEffect(() => {
     GetUserInfo(token, setUserLogado, setUserInfo )
+
+    const removeErrorInput = (event) => {
+      event.target.classList.remove('error')
+      event.target.placeholder = ''
+    };
+
+    inputPassword.current.addEventListener('focus', removeErrorInput)
+    inputNewPassword.current.addEventListener('focus', removeErrorInput)
+    inputConfirmNewPassword.current.addEventListener('focus', removeErrorInput)
+
   }, [])
 
   const navigate = useNavigate()
+  let hasErrorPassword = false
+
+  const inputPassword = useRef()
   const inputNewPassword = useRef()
+  const inputConfirmNewPassword = useRef()
+
   async function updatePassword() {
-    try {
-      await api.put(`/edit-password-user/${userLogado.id}`, {
-        newPassword: inputNewPassword.current.value
-      }, {
-        headers: {Authorization : `Bearer ${token}`}
-      })
-    } catch(err) {
-      console.log(err)
+
+    //validacao se campo de senha esta vazio
+    if (inputPassword.current?.value.trim() === '') {
+      inputPassword.current.value = ''
+      inputPassword.current.placeholder = 'Campo obrigatório'
+      inputPassword.current.classList.add('error')
     }
-    navigate('/menu')
+    //validacao se campo de nova senha esta vazio
+    if (inputNewPassword.current?.value.trim() === '') {
+      inputNewPassword.current.value = ''
+      inputNewPassword.current.placeholder = 'Campo obrigatório'
+      inputNewPassword.current.classList.add('error')
+    }
+    //validacao se campo de confirmar senha esta vazio
+    if (inputConfirmNewPassword.current?.value.trim() === '') {
+      inputConfirmNewPassword.current.value = ''
+      inputConfirmNewPassword.current.placeholder = 'Campo obrigatório'
+      inputConfirmNewPassword.current.classList.add('error')
+    }
+
+    //verifica se algum campo esta vazio e retorna erro
+    if (inputPassword.current?.value.trim() === '' || inputNewPassword.current?.value.trim() === '' || inputConfirmNewPassword.current?.value.trim() === '') {
+      hasErrorPassword = true
+      return
+    }
+
+    //verifica se a nova senha é igual a senha de confirmação
+    if (inputNewPassword.current?.value !== inputConfirmNewPassword.current?.value) {
+      inputNewPassword.current.value = ''
+      inputConfirmNewPassword.current.value = ''
+      inputNewPassword.current.placeholder = 'Senha não corresponde'
+      inputConfirmNewPassword.current.placeholder = 'Senha não corresponde'
+      inputNewPassword.current.classList.add('error')
+      inputConfirmNewPassword.current.classList.add('error')
+      hasErrorPassword = true
+    } 
+
+    if (!hasErrorPassword) {
+      try {
+        const userPassword = await api.post(`/verify-password/${userLogado.id}`, {
+          password: inputPassword.current.value
+        },{
+          headers: {Authorization : `Bearer ${token}`}
+        })
+        console.log(userPassword)
+      if (userPassword.status === 200) {
+        console.log('senha correta')
+      } else {
+        inputPassword.current.value = ''
+        inputPassword.current.placeholder = 'Senha Incorreta'
+        inputPassword.current.classList.add('error')
+        hasErrorPassword = true
+      }
+  
+      } catch (err) {
+        console.log(err)
+      }
+    }
+
+    if (!hasErrorPassword) {
+      try {
+        await api.put(`/edit-password-user/${userLogado.id}`, {
+          newPassword: inputNewPassword.current.value
+        }, {
+          headers: {Authorization : `Bearer ${token}`}
+        })
+      } catch(err) {
+        console.log(err)
+      }
+      navigate('/menu')
+    }
 
   }
-
 
   return <div className='componentPassword'>
     <div>
       <legend>Senha Atual</legend>
-      <input type="text" />
+      <input type="text" ref={inputPassword}/>
     </div>
 
     <div>
@@ -216,8 +290,8 @@ function ComponentPassword() {
     </div>
 
     <div>
-      <legend>Confirmar Senha</legend>
-      <input type="text" />
+      <legend>Confirmar Nova Senha</legend>
+      <input type="text" ref={inputConfirmNewPassword}/>
     </div>
     <button onClick={updatePassword}>Enviar</button>
   </div>;
