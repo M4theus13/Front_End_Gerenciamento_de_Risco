4ec99b2e259a47100229122c286d9684cd1bc4fd
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-03-31 13:38:45 -0300

    base router

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/",
+  base: "/Front_End_Gerenciamento_de_Risco/",
   server: {
     host: true // ou "0.0.0.0" para expor a rede
   }
