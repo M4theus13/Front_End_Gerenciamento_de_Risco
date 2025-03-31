7a5d539a7a3c8d7a982f453efd633254ff27c61b
Matheus Trabalho <matheusgarciajbs@gmail.com>
2025-03-31 13:25:28 -0300

    rota de deploy api

--- service/api.js
+++ service/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://api-gerenciamento-de-riscos.onrender.com'
+  baseURL: 'https://api.render.com/deploy/srv-cvir49juibrs73am9g60?key=6A3VkLKjKy0'
 }) 
 
 export default api
